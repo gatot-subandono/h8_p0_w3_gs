81d1e5a64bef318892340e66ef4c9233e1638ee7
gatot-subandono <gatot_tf@yahoo.com>
2019-11-14 18:42:11 +0800

    tambah baru

---
+++ exercise9_mean.js
@@ -0,0 +1,12 @@
+function cariMean(arr) {
+    var mean = 0;
+    for (var i=0; i<=arr.length-1; i++){
+        mean += arr[i];
+    }
+    return Math.ceil(mean / arr.length);    
+}
+console.log(cariMean([1, 2, 3, 4, 5])); // 3
+console.log(cariMean([3, 5, 7, 5, 3])); // 5
+console.log(cariMean([6, 5, 4, 7, 3])); // 5
+console.log(cariMean([1, 3, 3])); // 3
+console.log(cariMean([7, 7, 7, 7, 7])); // 7
